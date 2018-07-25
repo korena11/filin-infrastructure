63d1627614a06cf7b58fc9cfb7b64f98a9b930a6
elia <eliaa@campus.technion.ac.il>
2018-07-25 13:36:03 +0300

    Visualization Class Updated. - Example Included in VisualizationNew.py file. -- Example has two viewports with two layers each. Uses PointSet and PointSubSet and constant input color.
    Function `ToPolyData()` in PointSet and PointSubSet classes was modified to work with the new Visualization class.

--- Visualization Classes/Visualization.py
+++ Visualization Classes/Visualization.py
@@ -1,176 +1,206 @@
-# from mayavi import mlab
-# import matplotlib.pyplot as plt
-# from mayavi import mlab
-# from mayavi.core import lut_manager
-# from mayavi.mlab import quiver3d
-from numpy import tile, asarray, expand_dims, uint8
-from scipy.ndimage import morphology as morph
+# General Imports
+import numpy as np
+import vtk
 
-import SphericalCoordinatesProperty
+import VisualizationUtils
+# Framework Imports
 from BaseProperty import BaseProperty
 from ColorProperty import ColorProperty
-from NormalsProperty import NormalsProperty
 from PointSet import PointSet
 from PointSubSet import PointSubSet
 from SegmentationProperty import SegmentationProperty
-from TriangulationFactory import TriangulationFactory
-from TriangulationProperty import TriangulationProperty
 
 
-class Visualization:
-    
-    @classmethod
-    def __CreatePolyDataForRendering( cls, points, renderFlag, color = ( 0, 0, 0 ) ):
-        '''
-        
-        '''
-        if ( isinstance( points, BaseProperty ) ):
-            polyData = points.Points.ToPolyData()
-            numPoints = points.Points.Size
+class VisualizationVTK:
+    def __init__(self, number_of_view_ports=1, two_viewports_vertical_horizontal='H'):
+        # Setting Viewports
+        self.number_of_view_ports = number_of_view_ports
+        self.two_viewports_vertical_horizontal = two_viewports_vertical_horizontal
+        self.x_mins, self.x_maxs, self.y_mins, self.y_maxs = [0], [1], [0], [1]
+        self.set_number_of_viewports()
+
+        self.InitializeParametersVTK()
+
+    def set_number_of_viewports(self):
+        if self.number_of_view_ports not in (1, 2, 4):
+            raise ValueError('Number of supported viewports can be [1, 2, 4].')
+
         else:
-            polyData = points.ToPolyData()
-            numPoints = points.Size
-                        
-        if renderFlag == 'color':
-            polyData.point_data.scalars = asarray( 255 * tile( color, ( numPoints, 1 ) ), dtype = uint8 )        
-        elif renderFlag == 'externRgb' and isinstance( points, ColorProperty ):
-            polyData.point_data.scalars = points.RGB          
-        elif renderFlag == 'rgb' and ( isinstance( points, PointSet ) or isinstance( points, PointSubSet ) ) and points.RGB != None:            
-                polyData.point_data.scalars = points.RGB    
-        elif renderFlag == 'intensity' and ( isinstance( points, PointSet ) or isinstance( points, PointSubSet ) ) and points.Intensity != None:
-            polyData.point_data.scalars = points.Intensity            
-        elif renderFlag == 'height' and ( isinstance( points, PointSet ) or isinstance( points, PointSubSet ) ) and points.Z != None:
-            polyData.point_data.scalars = points.Z
-        elif renderFlag == 'height' and isinstance( points, BaseProperty ):
-            polyData.point_data.scalars = points.Points.Z
-        elif renderFlag == 'segmentation' and isinstance( points, SegmentationProperty ):
-            polyData.point_data.scalars = points.RGB     
-        elif renderFlag == 'parametericColor':
-            if len( color[0].shape ) == 1:
-                param = expand_dims( color[0], 1 )
+            if self.number_of_view_ports == 1:
+                pass
+
+            elif self.number_of_view_ports == 2:
+                if self.two_viewports_vertical_horizontal == 'V':
+                    self.x_mins = [0, .5]
+                    self.x_maxs = [0.5, 1]
+                    self.y_mins = [0, 0]
+                    self.y_maxs = [1, 1]
+                elif self.two_viewports_vertical_horizontal == 'H':
+                    self.x_mins = [0, 0]
+                    self.x_maxs = [1, 1]
+                    self.y_mins = [0, 0.5]
+                    self.y_maxs = [0.5, 1]
+                else:
+                    raise ValueError('Split Two Viewports Options: \'V\' - Vertical. \'H\' - Horizontal.')
+
             else:
-                param = color[0]
-            polyData.point_data.scalars = param              
-        else:  # display in some default color
-            print('rendering using default color') 
-            polyData.point_data.scalars = asarray( 255 * tile( ( 0.5, 0, 0 ), ( len( polyData.points ), 1 ) ), dtype = uint8 )
-            polyData.point_data.scalars.name = 'default'
-            
-        polyData.point_data.scalars.name = renderFlag            
-        
-        return polyData
-            
-    @classmethod    
-    def RenderPointSet( cls, points, renderFlag, _figure = None, color = ( 0, 0, 0 ), pointSize = 1.0, colorMap = None, colorBar = 0 ):
-        """
-        Render a PointSet object as points
-        Can use/render the following attributes
-            rgb color of each point 
-            intensity of each point
-            Normal for each point                 
-        
-        Args:
-            points (PointSet/PointSubSet):
-            
-            renderFlag (str): how to render the points. Can be one of:
-                'rgb': color the points using the original rgb values
-                'intensity': color the points using the original intensity values
-                'segmentation': color the points using segmentation results and given colormap
-                'height': color the points using the z coordinate 
-                'color': render all points using this color
-                'parametericColor': render accoding to given parameter
-            
-            color (3-tuple): color used to render points in case renderFlag = 'color'     
-            
-            _figure (mlab.figure, optional): Mayavi scene
-            
-            Optional Property objects:
-            
-            segmentationProperty (SegmentationProperty, optional)            
-            normalsProperty (NormalsProperty, optional)     
-            colorProperty
-            
-            pointSize (float, optional): size of the points when rendered
-        """                        
-        if ( points == None or ( ( isinstance( points, PointSet ) or isinstance( points, PointSubSet ) ) and points.Size == 0 ) or
-            ( isinstance( points, BaseProperty ) and points.Points.Size == 0 ) ):
-            return None  
-        
-        # Create a new figure
-        if _figure == None: 
-            _figure = mlab.figure( bgcolor = ( 0.8, 0.8, 0.8 ), fgcolor = ( 1, 1, 1 ) )
-                      
-        polyData = cls.__CreatePolyDataForRendering( points, renderFlag, color )
-        
-        if ( colorMap != None and colorMap in lut_manager.lut_mode_list() ):
-            s = mlab.pipeline.surface( polyData, colormap = colorMap )
-        else:
-            s = mlab.pipeline.surface( polyData )
+                self.x_mins = [0, .5, 0, .5]
+                self.x_maxs = [0.5, 1, 0.5, 1]
+                self.y_mins = [0, 0, .5, .5]
+                self.y_maxs = [0.5, 0.5, 1, 1]
 
-        s.actor.property.set( point_size = pointSize, representation = 'points' )
-        if colorBar == 1:
-            mlab.colorbar( orientation = 'horizontal', nb_labels = 5, label_fmt = '%.3f' )
-        
-        if isinstance( points, NormalsProperty ):
-            quiver3d( points.Points.X, points.Points.Y, points.Points.Z, points.dX, points.dY, points.dZ, scale_factor = 0.1, colormap = 'hsv' )
-        
-        return _figure        
-    
-    @classmethod
-    def RenderTriangularMesh( cls, points, renderFlag, _figure = None, color = ( 0, 0, 0 ), renderProperty = None, meshRepresentation = 'wireframe', colorMap = None ):
+    def InitializeParametersVTK(self):
         '''
-        
+        Initialize VTK parameters
         '''
-        if ( points == None or ( ( isinstance( points, PointSet ) or isinstance( points, PointSubSet ) ) and points.Size == 0 ) or
-            ( isinstance( points, BaseProperty ) and points.Points.Size == 0 ) ):
-            return None  
-        
-        if ( not isinstance( points, TriangulationProperty ) ):
-            points = TriangulationFactory.Delaunay2D( points )
-        
-        if ( isinstance( points.Points, SphericalCoordinatesProperty.SphericalCoordinatesProperty ) ):
-            points.Points = points.Points.XYZ
-        
-        # Create a new figure
-        if ( _figure == None ): 
-            _figure = mlab.figure( bgcolor = ( 0.5, 0.5, 0.5 ), fgcolor = ( 1, 1, 1 ) )
-        
-        if ( renderProperty == None ):
-            polyData = cls.__CreatePolyDataForRendering( points.Points, renderFlag, color )
+
+        self.vtkPolydataDictByViewport = {k: [] for k in range(self.number_of_view_ports)}
+        self.vtkCombinedPolyData = {}
+
+        self.vtkRenderWindow = vtk.vtkRenderWindow()  # Initialize a VTK render window
+        self.vtkRenderWindowInteractor = vtk.vtkRenderWindowInteractor()
+        self.vtkRenderWindowInteractor.SetRenderWindow(self.vtkRenderWindow)
+
+        self.listVTKRenderers = []
+        self.listVTKMappers = []
+        self.listVTKActors = []
+        for iRenderer in range(self.number_of_view_ports):
+            # Initialize VTK Renderers
+            self.listVTKRenderers.append(vtk.vtkRenderer())
+            # Add the VTK renderer to the VTK render window
+            self.vtkRenderWindow.AddRenderer(self.listVTKRenderers[iRenderer])
+            self.listVTKRenderers[iRenderer].SetViewport(self.x_mins[iRenderer], self.y_mins[iRenderer],
+                                                         self.x_maxs[iRenderer], self.y_maxs[iRenderer])
+            # Initialize VTK Mappers
+            self.listVTKMappers.append(vtk.vtkPolyDataMapper())
+
+            # Initialize VTK Actors
+            self.listVTKActors.append(vtk.vtkActor())
+            self.listVTKActors[iRenderer].SetMapper(self.listVTKMappers[iRenderer])
+            self.listVTKRenderers[iRenderer].AddActor(self.listVTKActors[iRenderer])
+
+    def __CreatePolyDataForRendering(self, input_data, render_flag, color=(255, 255, 255)):
+        if isinstance(input_data, BaseProperty):
+            polyData = input_data.Points.ToPolyData()
+            numPoints = input_data.Points.Size
         else:
-            polyData = cls.__CreatePolyDataForRendering( renderProperty.Points, renderFlag, color )
-            
-        polyData.polys = points.TrianglesIndices
-        
-        if ( colorMap != None and colorMap in lut_manager.lut_mode_list() ):
-            s = mlab.pipeline.surface( polyData, colormap = colorMap )
+            polyData = input_data.ToPolyData()
+            numPoints = input_data.Size
+
+        if render_flag == 'color':
+            scalars = np.asarray(np.tile(color, (numPoints, 1)), dtype=np.uint8)
+
+        elif render_flag == 'externRgb' and isinstance(input_data, ColorProperty):
+            scalars = input_data.RGB
+
+        elif render_flag == 'rgb' and (
+                isinstance(input_data, PointSet) or isinstance(input_data, PointSubSet)) and input_data.RGB != None:
+            scalars = input_data.RGB
+
+        elif render_flag == 'intensity' and (
+                isinstance(input_data, PointSet) or isinstance(input_data,
+                                                               PointSubSet)) and input_data.Intensity != None:
+            scalars = input_data.Intensity
+
+        elif render_flag == 'height' and (
+                isinstance(input_data, PointSet) or isinstance(input_data, PointSubSet)) and input_data.Z != None:
+            scalars = input_data.Z
+
+        elif render_flag == 'height' and isinstance(input_data, BaseProperty):
+            scalars = input_data.Points.Z
+
+        elif render_flag == 'segmentation' and isinstance(input_data, SegmentationProperty):
+            scalars = input_data.RGB
+
+        elif render_flag == 'parametericColor':
+            if len(color[0].shape) == 1:
+                scalars = np.expand_dims(color[0], 1)
+            else:
+                scalars = color[0]
+
+        else:  # display in some default color
+            print('Rendering using default color')
+            scalars = np.asarray(255 * np.tile((0.5, 0, 0), (len(polyData.points), 1)), dtype=np.uint8)
+            render_flag = 'default'
+
+        VisualizationUtils.AddScalarToPolydata(polyData, scalars, render_flag)
+        return polyData
+
+    # region  # Region: Binding Functions
+    def Bind(self, input_data, renderFlag='color', color=(255, 255, 255), new_layer=False):
+        self.BindTopLeftData(input_data, renderFlag, color, new_layer=new_layer)
+
+    # ----- #
+
+    def BindFirstHalf(self, input_data, renderFlag='color', color=(255, 255, 255), new_layer=False):
+        self.BindTopLeftData(input_data, renderFlag, color, new_layer=new_layer)
+
+    def BindSecondHalf(self, input_data, renderFlag='color', color=(255, 255, 255), new_layer=False):
+        self.BindTopRightData(input_data, renderFlag, color, new_layer=new_layer)
+
+    # ----- #
+
+    def BindTopLeftData(self, input_data, renderFlag='color', color=(255, 255, 255), new_layer=False):
+        self.InitializePointsArray(region_index=0, input_data=input_data, renderFlag=renderFlag, color=color,
+                                   new_layer=new_layer)
+
+    def BindTopRightData(self, input_data, renderFlag='color', color=(255, 255, 255), new_layer=False):
+        self.InitializePointsArray(region_index=1, input_data=input_data, renderFlag=renderFlag, color=color,
+                                   new_layer=new_layer)
+
+    def BindBottomLeftData(self, input_data, renderFlag='color', color=(255, 255, 255), new_layer=False):
+        self.InitializePointsArray(region_index=2, input_data=input_data, renderFlag=renderFlag, color=color,
+                                   new_layer=new_layer)
+
+    def BindBottomRightData(self, input_data, renderFlag='color', color=(255, 255, 255), new_layer=False):
+        self.InitializePointsArray(region_index=3, input_data=input_data, renderFlag=renderFlag, color=color,
+                                   new_layer=new_layer)
+
+    # endregion
+
+    def InitializePointsArray(self, region_index, input_data, renderFlag, color=(255, 255, 255), new_layer=False):
+
+        poly_data = self.__CreatePolyDataForRendering(input_data=input_data, render_flag=renderFlag, color=color)
+        if new_layer:
+            self.vtkPolydataDictByViewport[region_index].append(poly_data)
+            combined_polydata = VisualizationUtils.CombineVTKPolyDatas(self.vtkPolydataDictByViewport[region_index])
+            self.vtkCombinedPolyData[region_index] = combined_polydata
+            self.listVTKMappers[region_index].SetInputConnection(self.vtkCombinedPolyData[region_index].GetOutputPort())
         else:
-            s = mlab.pipeline.surface( polyData )
-        
-        s.actor.property.set( representation = meshRepresentation, line_width = 1.0 )
-        
-        return _figure
-    
-    @classmethod
-    def ShowPanorama( cls, panorama, colormap = None, _figure = None ):
-        """
-        Rendering a panorama image. In order to view the panorama use pyplot.show()
-        
-        :Args:
-            - panorama - A PanoramaProperty object
-            - colorMap - The color map used for rendering (optional)
-            - _figure - The figure to show the panorama on (optional)
-        """
-        if ( _figure == None ):
-            _figure = plt.figure()
-        
-        pano = morph.grey_erosion( uint8( panorama.PanoramaImage ), size = ( 3, 3 ) )
-#         pano = panorama.PanoramaImage
-        
-        plt.imshow( pano, colormap )
-        plt.axis( 'off' )
-        return _figure
-    
-    @staticmethod
-    def Show():
-        mlab.show()
+            self.vtkPolydataDictByViewport[region_index] = [poly_data]
+            self.vtkCombinedPolyData[region_index] = poly_data
+            self.listVTKMappers[region_index].SetInputData(self.vtkCombinedPolyData[region_index])
+
+        self.listVTKRenderers[region_index].ResetCamera()
+
+    # -----------------------------------------------------------------------------------------
+
+    def SetRenderWindowName(self, nameOfRenderWindow):
+        self.vtkRenderWindow.SetWindowName(str(nameOfRenderWindow))
+
+    def Show(self):
+        self.vtkRenderWindowInteractor.Initialize()
+        self.vtkRenderWindow.Render()
+        self.vtkRenderWindowInteractor.Start()
+
+
+if __name__ == '__main__':
+    points1 = (np.random.rand(1000, 3) - 0.5) * 1000.0
+    pointset1 = PointSet(points=points1)
+    pointsubset1 = PointSubSet(points=pointset1, indices=list(range(0, len(points1), 3)))
+
+    points2 = (np.random.rand(1000, 3) - 0.5) * 1000.0
+    pointset2 = PointSet(points=points2)
+    pointsubset2 = PointSubSet(points=pointset2, indices=list(range(0, len(points2), 3)))
+
+    number_of_viewports = 2
+    # Initialize VisualizationVTK Object. Number of Viewports = 1, 2, 4
+    visualization_object = VisualizationVTK(number_of_viewports, two_viewports_vertical_horizontal='V')
+    visualization_object.SetRenderWindowName("Test - One Viewport. PointSet. Uniform Color.")
+    visualization_object.BindFirstHalf(input_data=pointset1, renderFlag='color', color=(255, 0, 0), new_layer=False)
+    visualization_object.BindFirstHalf(input_data=pointset2, renderFlag='color', color=(0, 255, 0), new_layer=True)
+
+    visualization_object.BindSecondHalf(input_data=pointsubset1, renderFlag='color', color=(255, 0, 0), new_layer=False)
+    visualization_object.BindSecondHalf(input_data=pointsubset2, renderFlag='color', color=(0, 255, 0), new_layer=True)
+
+    visualization_object.Show()
