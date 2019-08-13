0427d1409f2dc4ca0120a1b5b4488221a177f0c0
Zachi <zachis@technion.ac.il>
2019-08-13 14:28:54 +0300

    2019/08/13 - computing curvature based on larger neighborhood with sector 8-connectivity-based analysis of neighbors

--- Properties/Saliency/MultiScaleSaliency/test_multSaliency.py
+++ Properties/Saliency/MultiScaleSaliency/test_multSaliency.py
@@ -3,13 +3,13 @@ from DataClasses.PointSet import PointSet
 from Properties.Segmentation.SegmentationProperty import SegmentationProperty
 from VisualizationClasses.VisualizationO3D import VisualizationO3D
 from Properties.Curvature.CurvatureProperty import CurvatureProperty
+from Properties.Tensors.TensorFactory import TensorFactory
 
-from numpy import arange, int_, vstack, unique, nonzero, array, zeros
+from numpy import arange, int_, vstack, unique, nonzero, array, zeros, ceil
 from numpy.linalg import norm
-from scipy.sparse import lil_matrix
+from scipy.sparse import lil_matrix, find
 from tqdm import tqdm
 
-import cProfile
 
 
 def arrangeInUniformCells(pntSet, cellSize):
@@ -74,7 +74,7 @@ def isValidCell(uniqueCells, labelMapping, label, minNumNeighbors=7):
     return getNeighbotingLabels(uniqueCells, labelMapping, label).shape[0] >= minNumNeighbors
 
 
-def getNeighbotingLabels(uniqueCells, labelMapping, label):
+def getNeighbotingLabels(uniqueCells, labelMapping, label, bufferSize, minNeighborsPerSector=1):
     """
     Getting the neighbors labels of a given cell (defined by its own label)
     :param uniqueCells: list of existing cells given as a list of their respective rows and cols (nx2, ndarray)
@@ -84,49 +84,109 @@ def getNeighbotingLabels(uniqueCells, labelMapping, label):
     """
     row, col = uniqueCells[label]
     neighbors = []
-    if row > 0 and labelMapping[row - 1, col] > 0:
-        neighbors.append(labelMapping[row - 1, col] - 1)
 
-    if row > 0 and col > 0 and labelMapping[row - 1, col - 1] > 0:
-        neighbors.append(labelMapping[row - 1, col - 1] - 1)
+    minRow = max([0, row - bufferSize])
+    minCol = max([0, col - bufferSize])
+    maxRow = min([labelMapping.shape[0], row + bufferSize])
+    maxCol = min([labelMapping.shape[1], col + bufferSize])
+    validSectors = 0
 
-    if row > 0 and col < labelMapping.shape[1] - 1 and labelMapping[row - 1, col + 1] > 0:
-        neighbors.append(labelMapping[row - 1, col + 1] - 1)
+    if row > 0:
+        rows, cols, labels = find(labelMapping[minRow:row, col])
 
-    if row < labelMapping.shape[0] - 1 and labelMapping[row + 1, col] > 0:
-        neighbors.append(labelMapping[row + 1, col] - 1)
+        if labels.shape[0] > 0:
+            neighbors.extend(labels - 1)
 
-    if row < labelMapping.shape[0] - 1and col > 0 and labelMapping[row + 1, col - 1] > 0:
-        neighbors.append(labelMapping[row + 1, col - 1] - 1)
+        if labels.shape[0] >= minNeighborsPerSector:
+            validSectors += 1
 
-    if row < labelMapping.shape[0] - 1 and col < labelMapping.shape[1] - 1 and labelMapping[row + 1, col + 1] > 0:
-        neighbors.append(labelMapping[row + 1, col + 1] - 1)
+    if row > 0 and col > 0:
+        rows, cols, labels = find(labelMapping[minRow:row, minCol:col])
 
-    if col > 0 and labelMapping[row, col - 1] > 0:
-        neighbors.append(labelMapping[row, col - 1] - 1)
+        if labels.shape[0] > 0:
+            neighbors.extend(labels - 1)
 
-    if col < labelMapping.shape[1] - 1 and labelMapping[row, col + 1] > 0:
-        neighbors.append(labelMapping[row, col + 1] - 1)
+        if labels.shape[0] >= minNeighborsPerSector:
+            validSectors += 1
 
-    return array(neighbors)
+    if row > 0 and col < labelMapping.shape[1] - 1:
+        rows, cols, labels = find(labelMapping[minRow:row, col + 1:maxCol + 1])
+
+        if labels.shape[0] > 0:
+            neighbors.extend(labels - 1)
+
+        if labels.shape[0] >= minNeighborsPerSector:
+            validSectors += 1
+
+    if row < labelMapping.shape[0] - 1:
+        rows, cols, labels = find(labelMapping[row + 1:maxRow + 1, col])
+
+        if labels.shape[0] > 0:
+            neighbors.extend(labels - 1)
+
+        if labels.shape[0] >= minNeighborsPerSector:
+            validSectors += 1
+
+    if row < labelMapping.shape[0] - 1and col > 0:
+        rows, cols, labels = find(labelMapping[row + 1:maxRow + 1, minCol:col])
+
+        if labels.shape[0] > 0:
+            neighbors.extend(labels - 1)
+
+        if labels.shape[0] >= minNeighborsPerSector:
+            validSectors += 1
+
+    if row < labelMapping.shape[0] - 1:
+        rows, cols, labels = find(labelMapping[row + 1:maxRow + 1, col + 1:maxCol + 1])
+
+        if labels.shape[0] > 0:
+            neighbors.extend(labels - 1)
+
+        if labels.shape[0] >= minNeighborsPerSector:
+            validSectors += 1
+
+    if col > 0:
+        rows, cols, labels = find(labelMapping[row, minCol:col])
+
+        if labels.shape[0] > 0:
+            neighbors.extend(labels - 1)
+
+        if labels.shape[0] >= minNeighborsPerSector:
+            validSectors += 1
+
+    if col < labelMapping.shape[1] - 1:
+        rows, cols, labels = find(labelMapping[row, col + 1:maxCol + 1])
+
+        if labels.shape[0] > 0:
+            neighbors.extend(labels - 1)
+
+        if labels.shape[0] >= minNeighborsPerSector:
+            validSectors += 1
+
+    if label in neighbors:
+        raise UserWarning('the cell with label :' + label + ' is found as its own neighbor')
+        a = 1
+
+    return array(neighbors, dtype=int), validSectors
 
 
-def computeUmbrellaCurvaturePerCell(segProp, tensor, cellNeighbors):
+def computeUmbrellaCurvaturePerCell(tensors, label, cellNeighbors):
     """
 
-    :param segProp:
-    :param tensor:
+    :param tensors:
+    :param label:
+    ":param c
     :return:
     """
-    neighboringPoints = vstack(list(map(lambda n: segProp.GetSegment(n).ToNumpy(), cellNeighbors)))
+    neighboringPoints = vstack(list(map(lambda n: tensors[n].reference_point, cellNeighbors)))
 
-    deltas = neighboringPoints - tensor.reference_point
+    deltas = neighboringPoints - tensors[label].reference_point
     normDeltas = norm(deltas, axis=1)
     deltas[:, 0] /= normDeltas
     deltas[:, 1] /= normDeltas
     deltas[:, 2] /= normDeltas
 
-    return tensor.plate_axis.reshape((1, -1)).dot(deltas.T).sum()
+    return tensors[label].plate_axis.reshape((1, -1)).dot(deltas.T).sum()
 
 
 if __name__ == '__main__':
@@ -134,37 +194,40 @@ if __name__ == '__main__':
     filename = 'Achziv_middle - Cloud_97'
     pntSet = IOFactory.ReadPts(path + filename + '.pts')
 
-    cellSize = 0.35
+    cellSize = 0.05
+    phenomSize = 0.35
+    buffer = int_(ceil(phenomSize / cellSize) / 2)
+    buffer = 1 if buffer == 0 else buffer
 
     rows, cols, labels, uniqueCells, cellLabels = arrangeInUniformCells(pntSet, cellSize)
     numLabels = labels.max() + 1
 
     segProp = SegmentationProperty(pntSet, labels)
 
-    VisualizationO3D.visualize_pointset(segProp)
+    # VisualizationO3D.visualize_pointset(segProp)
 
-    smoothSegProp = smoothPointsWithinEachCell(segProp)
+    # smoothSegProp = smoothPointsWithinEachCell(segProp)
+    smoothSegProp = segProp
 
-    from TensorFactory import TensorFactory
     tensors = list(map(lambda l: TensorFactory.tensorFromPoints(smoothSegProp.GetSegment(l)),
                        tqdm(range(numLabels), desc='Computing tensors for each cell')))
 
-    neighbors = list(map(lambda l: getNeighbotingLabels(uniqueCells, cellLabels, l),
-                         tqdm(range(numLabels), desc='retrieving neighbors for all cells')))
-    validCells = nonzero(list(map(lambda n: n.shape[0] >= 7, neighbors)))[0]
+    neighbors = array(list(map(lambda l: getNeighbotingLabels(uniqueCells, cellLabels, l, buffer),
+                           tqdm(range(numLabels), desc='retrieving neighbors for all cells'))))
+    validCells = neighbors[:, 1]
+    neighbors = neighbors[:, 0]
+    validCells = nonzero(validCells >= 7)[0]
     # validCells = nonzero(list(map(lambda l: isValidCell(uniqueCells, cellLabels, l, 7),
     #                               tqdm(range(numLabels), 'checking validity of each cell'))))[0]
 
     curvatures = zeros((numLabels, ))
-    curvatures[validCells] = list(map(lambda l: computeUmbrellaCurvaturePerCell(smoothSegProp,
-                                                                                tensors[l], neighbors[l]),
+    curvatures[validCells] = list(map(lambda l: computeUmbrellaCurvaturePerCell(tensors, l, neighbors[l]),
                                       tqdm(validCells, desc='computing curvatures for each valid cell')))
     pntCurvatures = zeros((pntSet.Size, ))
     list(map(lambda i: pntCurvatures.__setitem__(segProp.GetSegmentIndices(validCells[i]), curvatures[validCells[i]]),
              tqdm(range(validCells.shape[0]), desc='Updating points curvatures')))
 
     curveProp = CurvatureProperty(smoothSegProp.Points, umbrella_curvature=pntCurvatures)
-
 
     visObj = VisualizationO3D()
     visObj.visualize_property(curveProp)
