Working Level set method: region + GVF + geodesic Wrong GVF computation Multichannel not checked

LevelSets/LevelSetFlow.py
@@ -385,7 +385,7 @@ class LevelSetFlow:
         :param radius: if the curve is a circle, the radius should be specified.
         :param center_pt: if the curve is a circle, the center point should be specified.
         :param reularization_note: regularization note for heaviside function
-        :param function_type: 'circle' (default); 'vertical' or 'horizontal' (for open contours)
+        :param function_type: 'circle' (default); 'vertical' or 'horizontal' (for open contours); 'ellipse'; 'sin'
 
         :type processing_props: dict
         :type radius: int
@@ -395,7 +395,7 @@ class LevelSetFlow:
         :type regularization_note: int 0,1,2
 
         """
-        processing_props = {'gradientType': 'L1', 'sigma': 2.5, 'ksize': 5}
+        processing_props = {'gradientType': 'L1', 'sigma': 2.5, 'ksize': 5, 'resolution': 1.}
         if 'processing_props' in kwargs:
             processing_props.update(kwargs['processing_props'])
         img_height, img_width = self.img().shape[:2]
@@ -403,11 +403,20 @@ class LevelSetFlow:
         center_pt = kwargs.get('center_pt', [np.int(img_height / 2), np.int(img_width / 2)])
         func_type = kwargs.get('function_type', 'circle')
 
+
         regularization = kwargs.get('regularization_note', 0)
 
         if func_type == 'circle':
             phi = LevelSetFunction.dist_from_circle(center_pt, radius, (img_height, img_width),
-                                                    ksize=processing_props['ksize'])
+                                                    resolution=processing_props['resolution'])
+        elif func_type == 'ellipse':
+            phi = LevelSetFunction.dist_from_ellipse(center_pt, radius, (img_height, img_width),
+                                                    resolution=processing_props['resolution'])
+        elif func_type == 'egg_crate':
+            phi = LevelSetFunction.dist_from_eggcrate(amplitude=kwargs['amplitude'], func_shape=(img_height, img_width), resolution=kwargs['resolution'])
+
+        elif func_type == 'periodic_circles':
+            phi = LevelSetFunction.dist_from_circles(kwargs['dx'], kwargs['dy'], radius, func_shape=(img_height, img_width))
 
         if np.all(self.__Phi[0].value == 0):
             self.__Phi = []
@@ -457,17 +466,26 @@ class LevelSetFlow:
 
         """
         self.__g = g
-        self.__g_x, self.__g_y = mt.computeImageDerivatives(g, 1, **kwargs)
+        self.__g_x, self.__g_y = mt.computeImageDerivatives_numeric(g, 1, **kwargs)
 
-    def init_f(self, f, **kwargs):
+    def init_vector_field(self, f, mu=0.2, iterations=80, **kwargs):
         """
-        Initializes the g function (edge function)
-        :param g: the function
-        :param kwargs: gradient process dictionary
+        Initializes the vector field (gradient vector flow)
+
+        Computes the GVF of an edge map f according the the paper of :cite:`Xu.Prince1998`
+
+        :param f: the edge map according to which the GVF is computed
+        :param mu: the GVF regularization coefficient. Default: 0.2 (according to the paper).
+        :param iterations: the number of iterations that will be computed. Default: 80
+        :param kwargs: gradient process dictionary (sigma, ksize, resolution)
 
         """
+
+        # normalize f to the range [0,1]
+        f = cv2.normalize(f.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
+        
         self.__f = f
-        self.__f_x, self.__f_y = mt.computeImageDerivatives(f, 1, **kwargs)
+        self.__f_x, self.__f_y = mt.computeImageDerivatives_numeric(f, 1, **kwargs)
 
     def init_region(self, region):
         """
@@ -477,7 +495,7 @@ class LevelSetFlow:
 
         """
 
-        region = cv2.normalize(region.astype('float'), None, -1.0, 1.0, cv2.NORM_MINMAX)
+        # region = cv2.normalize(region.astype('float'), None, -1.0, 1.0, cv2.NORM_MINMAX)
         self.__region = region
 
     def update_region(self, new_region):
@@ -572,12 +590,17 @@ class LevelSetFlow:
         if flow_type == 'geodesic':
             # !! pay attention, if self.g is constant - then this flow is actually curvature flow !!
 
+            while np.any(function.norm_nabla > 1e10):
+                new_value = function.value
+                new_value[function.norm_nabla > 1e10] = np.sign(new_value[function.norm_nabla > 1e10]) * 1
+                function.update(new_value)
+
             flow = self.g * function.kappa * function.norm_nabla + (self.g_x * function._x + self.g_y *
                                                                     function._y)
-            if open_flag:
-                psi_t = self.g * self.function.kappa * self.psi.norm_nabla + (
-                        self.g_x * self.psi._x + self.g_y * self.psi._y)
-                self.psi.update(self.psi.value - psi_t, regularization_note=processing_props['regularization'])
+            # if open_flag:
+            #     psi_t = self.g * self.function.kappa * self.psi.norm_nabla + (
+            #             self.g_x * self.psi._x + self.g_y * self.psi._y)
+            #     self.psi.update(self.psi.value - psi_t, regularization_note=processing_props['regularization'])
 
         if flow_type == 'band':
             vb = self.__compute_vb(**processing_props)
@@ -744,7 +767,7 @@ class LevelSetFlow:
         #  vb[np.where(phi != 0)] = 0
         return vb * self.phi().kappa
 
-    def __compute_vt(self, vectorField, verbose=False, **kwargs):
+    def __compute_vt(self, vectorField, verbose=False, phi_index =0, mu=0.2, **kwargs):
         """
         Computes the vector field derivative in each direction
 
@@ -755,6 +778,8 @@ class LevelSetFlow:
         :param edge_map:
         :param kappa: curvature map
         :param verbose: show vector field
+        :param phi_index: the index of the level set function to which the vector field is computed
+        :param mu: weighting index for the GVF
 
         :type verbose: bool
 
@@ -770,18 +795,18 @@ class LevelSetFlow:
         """
 
         # compute the derivatives of the edge map at each direction
-        nabla_edge = mt.computeImageGradient(self.f, **kwargs)
+        nabla_edge = mt.computeImageGradient_numeric(self.f, **kwargs)
 
         # compute the Laplacian of the vector field
         laplacian_vx = cv2.Laplacian(vectorField[:, :, 0], cv2.CV_64F)
         laplacian_vy = cv2.Laplacian(vectorField[:, :, 1], cv2.CV_64F)
 
         # compute the functions that are part of the vt computation
-        g = np.exp(-nabla_edge / (self.phi().kappa + EPS) ** 2)
+        g = np.exp(-nabla_edge / (self.phi(phi_index).kappa + EPS) ** 2)
         h = 1 - g
 
-        vx_t = g * laplacian_vx - h * (vectorField[:, :, 0] - self.f)
-        vy_t = g * laplacian_vy - h * (vectorField[:, :, 1] - self.f)
+        vx_t = mu * laplacian_vx - (vectorField[:, :, 0] - self.f_x)*(self.f_x**2 + self.f_y**2)
+        vy_t = mu * laplacian_vy - (vectorField[:, :, 1] - self.f_y)*(self.f_x**2 + self.f_y**2)
 
         if verbose == True:
             # plt.quiver(vx_t, vy_t, scale=25)
@@ -791,7 +816,7 @@ class LevelSetFlow:
             plt.figure()
             plt.imshow(vectorField[:, :, 1])
 
-        return np.stack((-vx_t, -vy_t), axis=2)
+        return np.stack((vx_t, vy_t), axis=2)
 
     def __compute_vo(self, **kwargs):
         """
@@ -886,12 +911,11 @@ class LevelSetFlow:
         # with writer.saving(fig, "level_set.mp4", 100):
         for iteration in range(iterations):
             print(iteration)
-            if verbose:
-                print(iteration)
-                if iteration > 26:
-                    print('hello')
+
+
+            if iteration ==7 :
+                print('hello')
             intrinsic = np.zeros(self.img().shape[:2])
-            extrinsic = np.zeros(self.img().shape[:2])
 
             # ---------- intrinsic movement ----------
             if mumford_shah_flag:
@@ -902,7 +926,7 @@ class LevelSetFlow:
                 if flow_types[item] == 0:
                     continue
                 for i in range(self.num_ls):
-                    intrinsic += flow_types[item] * self.flow(item, self.phi(i), open_flag, processing_props)
+                  intrinsic += flow_types[item] * self.flow(item, self.phi(i), open_flag, processing_props)
 
                 if verbose:
                     if np.any(intrinsic > 20):
@@ -913,19 +937,19 @@ class LevelSetFlow:
                 intrinsic += region_w * self.region * self.phi(j).norm_nabla
 
             # ---------------extrinsic movement ----------
-            v = np.stack((self.f_x, self.f_y), axis=2)
-            vt = self.__compute_vt(v, verbose=verbose, **processing_props)
-            v += vt
             for k in range(self.num_ls):
+                v = np.stack((self.f_x, self.f_y), axis=2)
+                vt = self.__compute_vt(v, verbose=verbose, phi_index=k, **processing_props)
+                v += vt
                 extrinsic = (v[:, :, 0] * self.phi(k)._x + v[:, :, 1] * self.phi(k)._y) * gvf_w
 
                 # for constrained contours
                 extrinsic += self.__compute_vo() * vo_w
                 phi_t = self.step * (intrinsic - extrinsic)
                 # reinitializtion every 5 iterations:
-                if iteration % 10 == 0 and iteration != 0:
+                if iteration % 10== 0 and iteration != 0:
                     self.phi(k).reinitialization(phi_t)
-                    pass
+
                 else:
                     self.phi(k).move_function(phi_t)
 
@@ -962,6 +986,7 @@ class LevelSetFlow:
                         l_curve, ax = mt.draw_contours(self.phi(i).value, ax, img=img_showed, hold=False,
                                                        color=colors[i], blob_size=blob_size)
             # writer.grab_frame()
+            plt.pause(.0001)
         plt.show()
         print('Done')
         return l_curve

LevelSets/LevelSetFunction.py
@@ -42,7 +42,7 @@ class LevelSetFunction(object):
         :rtype: LevelSetFunction
 
         """
-        processing_props = {'gradientType': 'L1', 'sigma': 2.5, 'ksize': 1}
+        processing_props = {'gradientType': 'L1', 'sigma': 2.5, 'ksize': 1, 'resolution': 0.5}
         self.__processing_props = processing_props
         self.__value = function
         self.__ls_derivatives_curvature()
@@ -194,7 +194,9 @@ class LevelSetFunction(object):
         :param regularization_note: the regularization note (0,1, or 2) for the Heavisdie and the Dirac-Delta functions.
 
         """
-        new_function = cv2.normalize(new_function, None, -1., 1, cv2.NORM_MINMAX)
+        new_function[new_function > np.percentile(new_function, 97)] = np.percentile(new_function, 95)
+        new_function[new_function< np.percentile(new_function, 2)] = np.percentile(new_function, 5)
+
         self.__value = new_function
         self.__ls_derivatives_curvature()
         if 'regularization_note' in list(kwargs.keys()):
@@ -250,15 +252,29 @@ class LevelSetFunction(object):
         """
 
         phi_temp = LevelSetFunction(self.value + dphi)
+        # statistical normalization
+        value_temp = phi_temp.value
+
+        value_temp[phi_temp.value > np.percentile(phi_temp.value, 95)] = np.percentile(phi_temp.value, 95)
+        value_temp[phi_temp.value < np.percentile(phi_temp.value, 5)] = np.percentile(phi_temp.value, 5)
+        phi_temp.update(value_temp)
+
         S_phi = self.value / np.sqrt(self.value ** 2 + 1)
 
-        phi_t = S_phi * (1 - phi_temp.norm_nabla)
+        phi_t = S_phi * (1- phi_temp.norm_nabla)
 
         self.update(self.value + phi_t)
+        while np.any(self.norm_nabla) > 1e10:
+            new_value = self.value
+            new_value[self.norm_nabla > 1e10] = np.sign(new_value[self.norm_nabla > 1e10]) * 1
+            self.update(new_value)
+
+
+
         
         
     @staticmethod
-    def dist_from_circle(center_pt, radius, func_shape, **kwargs):
+    def dist_from_circle(center_pt,  radius, func_shape, resolution=.5):
         r"""
         Build a Lipshitz distance function from a circle, with a specific size
 
@@ -270,7 +286,7 @@ class LevelSetFunction(object):
         :param center_pt:  center of the circle
         :param radius:  radius of the circle
         :param func_shape: size of the function (height, width)
-        :param ksize: the kernel size for later processing. Default: 5
+        :param resolution: the kernel size for later processing. Default: 0.5
 
         :type center_pt: tuple
         :type radius: int
@@ -283,19 +299,118 @@ class LevelSetFunction(object):
         """
         height = func_shape[0]
         width = func_shape[1]
-        ksize = kwargs.get('ksize', 5)
         x = np.arange(width)
         y = np.arange(height)
-        xx, yy = np.meshgrid(ksize * x, ksize * y)
-        x_x0 = (xx - center_pt[1] * ksize) # (x-x0)
-        y_y0 =  (yy - center_pt[0] * ksize) #(y-y0)
+        xx, yy = np.meshgrid(resolution * x, resolution* y)
+        x_x0 = (xx - center_pt[1] * resolution) # (x-x0)
+        y_y0 =  (yy - center_pt[0] * resolution) #(y-y0)
 
         phi = radius - np.sqrt(x_x0 ** 2 + y_y0 ** 2)
-        # gradient should equal 1
-        # norm_nabla = mt.computeImageGradient(phi, ksize=ksize, gradientType='L2')
-        # phi /= norm_nabla**2
-        phi = cv2.normalize(phi, None, -1, 1, cv2.NORM_MINMAX)
+
         return phi
+    @staticmethod
+    def dist_from_circles(dx, dy, radius, func_shape, resolution=.5):
+        """
+        Build a Lipshitz distance function with repetitive circles
+
+        .. math::
+
+           \phi(x,y,t) < 0 \quad \text{for } (x,y) \not\in \Omega
+
+        :param dx: distance between circles on x
+        :param dy: distance between circles on y
+        :param radius: radius of the circles
+        :param func_shape: size of the function (height, width)
+        :param resolution: grid size
+
+        :return: a level set function that its zero-set is the defined circles (approximately)
+        """
+        import skfmm
+        phi = -np.ones(func_shape)
+        center_x = np.arange(dx + radius, func_shape[1], dx + radius)
+        center_y = np.arange(dy + radius, func_shape[0], dy + radius)
+
+        for i in center_y:
+            for j in center_x:
+                phi_temp = LevelSetFunction.dist_from_circle((i,j), radius, func_shape, resolution=resolution)
+                phi[int(i-radius):int(i+radius),int(j-radius):int(j+radius)] = phi_temp[int(i-radius):int(i+radius),int(j-radius):int(j+radius)]
+
+
+        return skfmm.distance(phi)
+
+
+
+
+
+    @staticmethod
+    def dist_from_ellipse(center_pt, axes, func_shape, resolution=.5):
+        r"""
+        Build a Lipshitz distance function from an ellipse, with a specific size
+
+        .. math::
+
+           \phi(x,y,t) < 0 \quad \text{for } (x,y) \not\in \Omega
+
+
+        :param center_pt:  center of the ellipse
+        :param axes:  axes sizes of the ellipse
+        :param func_shape: size of the function (height, width)
+        :param resolution: the kernel size for later processing. Default: 5
+
+        :type center_pt: tuple
+        :type radius: int
+        :type func_shape: tuple
+
+        :return: a level set function that its zero-set is the defined ellipse (approximately)
+
+        :rtype: np.array
+
+        """
+        height = func_shape[0]
+        width = func_shape[1]
+        x = np.arange(width)
+        y = np.arange(height)
+        xx, yy = np.meshgrid(resolution * x, resolution * y)
+        x_x0 = (xx - center_pt[1] * resolution)  # (x-x0)
+        y_y0 = (yy - center_pt[0] * resolution)  # (y-y0)
+
+        phi = np.sqrt((x_x0 / axes[0])** 2 + (y_y0/axes[1]) ** 2) - 1
+
+        return phi
+    @staticmethod
+    def dist_from_eggcrate(amplitude, func_shape, resolution=.5):
+        r"""
+        Build a Lipshitz distance function with a sine function, with a specific size
+
+        .. math::
+
+           \phi(x,y,t) < 0 \quad \text{for } (x,y) \not\in \Omega
+
+
+        :param amplitude:  the amplitude of the egg grate
+        :param func_shape: size of the function (height, width)
+        :param resolution: the kernel size for later processing. Default: .5
+
+        :type center_pt: tuple
+        :type radius: int
+        :type func_shape: tuple
+
+        :return: a level set function that its zero-set is the defined circle (approximately)
+
+        :rtype: np.array
+        """
+        height = func_shape[0]
+        width = func_shape[1]
+        x = np.arange(-width/2, width/2, resolution)
+        y = np.arange(-height/2, height/2, resolution)
+        xx, yy = np.meshgrid(x, y)
+        sin_xx = np.sin(xx )
+        sin_yy = np.sin(yy )
+
+        return xx**2 + yy**2 - amplitude * (sin_xx**2 + sin_yy**2)
+
+
+
 
     def compute_heaviside(self, **kwargs):
         r"""

Utils/MyTools.py
@@ -219,7 +219,45 @@ def computeImageDerivatives(img, order, **kwargs):
     else:
         return img_x, img_y
 
-def computeImageDerivatives_numeric(img, order, **kwargs):
+def computeImageGradient_numeric(img, gradientType='L1', ksize=3, sigma=1, resolution=1, blur_window=(0,0), **kwargs):
+    """
+
+        :param img: the image to which the derivatives should be computed
+        :param gradientType: 'L1' L1 norm of grad(I); 'L2' L2-norm of grad(I); 'LoG' Laplacian of gaussian
+        :param ksize: size of the differentiation window
+        :param resolution: kernel resolution
+        :param sigma: sigma for gaussian blurring. Default: 1. If sigma=0 no smoothing is carried out
+        :param blur_window: tuple of window size for blurring
+
+        :type img: np.array
+        :type gradientType: str
+        :type ksize: int
+        :type resolution: float
+        :type sigma: float
+        :type blur_window: tuple
+
+        :return: an image of the gradient magnitude
+        :rtype: np.array
+    """
+
+    gradient = None
+
+    # img = cv2.GaussianBlur(I, (ksize, ksize), sigma)
+
+    # compute image gradient (numeric)
+    dx, dy = computeImageDerivatives_numeric(img, 1, ksize, sigma, resolution,blur_window)
+
+    if gradientType == 'L1':
+        gradient = cv2.GaussianBlur((np.abs(dx) + np.abs(dy)), (ksize, ksize), sigma)  # L1-norm of grad(I)
+    elif gradientType == 'L2':
+        gradient = cv2.GaussianBlur(np.sqrt(dx ** 2 + dy ** 2), (ksize, ksize), sigma)
+    elif gradientType == 'LoG':
+        gradient = filters.gaussian_laplace(img, sigma)
+
+    # return cv2.normalize((gradient).astype('float'), None, 0.0,1.0, cv2.NORM_MINMAX)
+    return gradient
+
+def computeImageDerivatives_numeric(img, order, ksize=3, sigma=1., resolution=1., blur_window=(0,0), **kwargs):
     """
         Computes numeric image derivatives up to order 2.
 
@@ -230,24 +268,25 @@ def computeImageDerivatives_numeric(img, order, **kwargs):
         :param sigma: sigma for gaussian blurring. Default: 1. If sigma=0 no smoothing is carried out
         :param blur_window: tuple of window size for blurring
 
+        :type img: np.array
+        :type order: int
+        :type ksize: int
+        :type resolution: float
+        :type sigma: float
+        :type blur_window: tuple
+
 
         :return: tuple of the derivatives in the following order: (dx, dy, dxx, dyy, dxy)
 
         .. note::
             Eq. numbers from Amit Baruch Dissertation
     """
-    params = {'ksize': 3,
-              'resolution': 1,
-              'sigma': 1.,
-              'blur_window': (0, 0)}
-    params.update(kwargs)
-    ksize = np.int(params['ksize'])
 
     img = np.float64(img)
 
     # if blurring is required before differentiation
-    if params['sigma'] != 0:
-        img = cv2.GaussianBlur(img, params['window'], params['sigma'])
+    if sigma != 0:
+        img = cv2.GaussianBlur(img, blur_window, sigma)
 
     floord = np.int(ksize)
 
@@ -256,12 +295,12 @@ def computeImageDerivatives_numeric(img, order, **kwargs):
     # x direction
     I1 = np.hstack((img[: , floord: ], img[:, - floord:]))
     I2 = np.hstack((img[:, : floord], img[:, : - floord]))
-    Zx = (I1 - I2) / (2 * params['resolution'] * np.ceil(ksize))
+    Zx = (I1 - I2) / (2 * resolution * np.ceil(ksize))
 
     # y direction
     I3 = np.vstack((img[floord: , :], img[-floord:, :]))
     I4 = np.vstack((img[: floord, :], img[: -floord, :]))
-    Zy = (I3 - I4) / (2 * params['resolution'] * ksize)
+    Zy = (I3 - I4) / (2 * resolution * ksize)
 
     # second order derivatives
     if order == 2:
@@ -276,9 +315,9 @@ def computeImageDerivatives_numeric(img, order, **kwargs):
         I8 = np.vstack((I8a, I8a[- floord:, :]))
 
         # eq. (3-40)
-        Zxx = (-2 * img + I1 + I2) / (ksize * params['resolution']) ** 2
-        Zyy = (-2 * img + I3 + I4) / (ksize * params['resolution']) ** 2
-        Zxy = (I7 + I6 - I5 - I8) / (2 * ksize * params['resolution']) ** 2
+        Zxx = (-2 * img + I1 + I2) / (ksize * resolution) ** 2
+        Zyy = (-2 * img + I3 + I4) / (ksize * resolution) ** 2
+        Zxy = (I7 + I6 - I5 - I8) / (2 * ksize * resolution) ** 2
 
         return Zx, Zy, Zxx, Zyy, Zxy
 